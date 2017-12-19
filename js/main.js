4b034e69eb42a67ea2b66d0fb8b00598ef5716e1
wangfengshan <1552388929@qq.com>
2017-12-19 10:58:25 +0800

    js完成
    js完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
  /*Initialize Swiper*/
 var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
